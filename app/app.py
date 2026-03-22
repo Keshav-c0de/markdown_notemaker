2c32b392f732abd3167e5b55ff4e6d5f7a05d51a
Keshav Bhardwaj <235341218+Keshav-c0de@users.noreply.github.com>
2026-03-22 18:30:59 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -21,7 +21,7 @@ app = FastAPI(lifespan= lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # For development; change to specific URLs for production
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
